ea005cf323e4e03adb4969fa749cdd1493fa8c2f
Anjiurine <q78kg.1ffrj6@outlook.com>
2023-10-29 22:47:03 +0800

    .

--- api/index.py
+++ api/index.py
@@ -15,7 +15,8 @@ class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         img_path = self.path[1:]
         origin_url = env_origin_url + img_path
-        response = requests.get(origin_url)
+        headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36'}
+        response = requests.get(origin_url, stream=True, headers=headers)
         image = cv2.imdecode(np.frombuffer(response.content, np.uint8), 1)
         _, ext = os.path.splitext(img_path)
         if ext in [".jpg", ".jpeg"]:
