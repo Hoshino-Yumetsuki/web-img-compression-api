baf65dff185b84afaba863a87fff6b1ce201071c
Anjiurine <q78kg.1ffrj6@outlook.com>
2023-10-30 19:44:04 +0800

    .

--- api/index.py
+++ api/index.py
@@ -5,31 +5,26 @@ import requests
 import io
 import os
 
-env_origin_url = "https://pixiv-img.anjiurine.top/"
-env_jpeg_quality = "80"
-env_webp_quality = "80"
-env_png_compression = "6"
-
 class handler(BaseHTTPRequestHandler):
 
     def do_GET(self):
         img_path = self.path[1:]
-        origin_url = env_origin_url + img_path
+        origin_url = "https://pixiv-img.anjiurine.top/" + img_path
         headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36 Edg/118.0.2088.69'}
         response = requests.get(origin_url, stream=True, headers=headers)
         image = cv2.imdecode(np.frombuffer(response.content, np.uint8), 1)
         _, ext = os.path.splitext(img_path)
         if ext in [".jpg", ".jpeg"]:
             # 设置jpg格式图片的压缩质量（数字越大质量越高，最大为100）
-            encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), env_jpeg_quality]
+            encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 80]
             img_type = "image/jpeg"
         elif ext == ".webp":
             # 设置webp格式图片的压缩质量（数字越大质量越高，最大为100）
-            encode_param = [int(cv2.IMWRITE_WEBP_QUALITY), env_webp_quality]
+            encode_param = [int(cv2.IMWRITE_WEBP_QUALITY), 80]
             img_type = "image/webp"
         elif ext == ".png":
             # 设置png格式图片的压缩质量（数字越大质量越高，最大为9）
-            encode_param = [int(cv2.IMWRITE_PNG_COMPRESSION), env_png_compression]
+            encode_param = [int(cv2.IMWRITE_PNG_COMPRESSION), 6]
             img_type = "image/png"
         else:
             self.send_response(404)
