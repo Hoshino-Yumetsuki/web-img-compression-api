6c7b22127985133b34844bf9511e455d0f7355b6
Anjiurine <q78kg.1ffrj6@outlook.com>
2023-11-11 14:12:04 +0800

    .

--- api/index.py
+++ api/index.py
@@ -18,17 +18,17 @@ class handler(BaseHTTPRequestHandler):
             image = cv2.imdecode(np.frombuffer(response.content, np.uint8), 1)
             _, ext = os.path.splitext(img_path)
             if ext in [".jpg", ".jpeg"]:
-                env_jpg_quality = os.environ.get("JPG_QUALITY")
+                env_jpg_quality = int(os.environ.get("JPG_QUALITY"))
                 # 设置jpg格式图片的压缩质量（数字越大质量越高，最大为100）
                 encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), env_jpg_quality]
                 img_type = "image/jpeg"
             elif ext == ".webp":
-                env_webp_quality = os.environ.get("WEBP_QUALITY")
+                env_webp_quality = int(os.environ.get("WEBP_QUALITY"))
                 # 设置webp格式图片的压缩质量（数字越大质量越高，最大为100）
                 encode_param = [int(cv2.IMWRITE_WEBP_QUALITY), env_webp_quality]
                 img_type = "image/webp"
             elif ext == ".png":
-                env_png_compression = os.environ.get("PNG_COMPRESSION")
+                env_png_compression = int(os.environ.get("PNG_COMPRESSION"))
                 # 设置png格式图片的压缩质量（数字越大压缩比越大，最大为9）
                 encode_param = [int(cv2.IMWRITE_PNG_COMPRESSION), env_png_compression]
                 img_type = "image/png"
