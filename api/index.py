7624eaf862d87738f527753b60775e6dc076b352
Anjiurine <q78kg.1ffrj6@outlook.com>
2023-12-04 22:33:33 +0800

    support active mod

--- api/index.py
+++ api/index.py
@@ -4,38 +4,68 @@ import numpy as np
 import requests
 import io
 import os
+from urllib.parse import unquote
 
 class handler(BaseHTTPRequestHandler):
 
     def do_GET(self):
-        img_path = self.path[1:]
-        env_origin_url = os.environ.get("ORIGIN_URL")
-        origin_url = env_origin_url + img_path
-        env_referer_url = os.environ.get("REFERER_URL")
-        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36 Edg/118.0.2088.76', 'Referer': env_referer_url}
+        active_mode = os.environ.get("ACTIVE_MODE", "").lower() in ["true", "True"]
 
-        try:
-            response = requests.get(origin_url, stream=True, headers=headers)
-            response.raise_for_status()
-            image = cv2.imdecode(np.frombuffer(response.content, np.uint8), 1)
-            _, ext = os.path.splitext(img_path)
+        if active_mode:
+            img_url = unquote(self.path[1:])
+            headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36 Edg/118.0.2088.76'}
 
-            encode_param, img_type = self.get_encode_param_and_type(ext)
+            try:
+                response = requests.get(img_url, stream=True, headers=headers)
+                response.raise_for_status()
+                image = cv2.imdecode(np.frombuffer(response.content, np.uint8), 1)
+                _, ext = os.path.splitext(img_url)
 
-            output = io.BytesIO()
-            result, data = cv2.imencode(ext, image, encode_param)
-            output.write(data)
-            output.seek(0)
-            self.send_response(200)
-            self.send_header('Content-type', img_type)
-            self.end_headers()
-            self.wfile.write(output.read())
+                encode_param, img_type = self.get_encode_param_and_type(ext)
 
-        except requests.exceptions.HTTPError as e:
-            self.send_error(400, message="Bad Request: {}".format(str(e)))
+                output = io.BytesIO()
+                result, data = cv2.imencode(ext, image, encode_param)
+                output.write(data)
+                output.seek(0)
+                self.send_response(200)
+                self.send_header('Content-type', img_type)
+                self.end_headers()
+                self.wfile.write(output.read())
 
-        except Exception as e:
-            self.send_error(500, message="Internal Server Error: {}".format(str(e)))
+            except requests.exceptions.HTTPError as e:
+                self.send_error(400, message="Bad Request: {}".format(str(e)))
+
+            except Exception as e:
+                self.send_error(500, message="Internal Server Error: {}".format(str(e)))
+        else:
+            img_path = unquote(self.path[1:])
+            env_origin_url = os.environ.get("ORIGIN_URL")
+            origin_url = env_origin_url + img_path
+            env_referer_url = os.environ.get("REFERER_URL")
+            headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36 Edg/118.0.2088.76', 'Referer': env_referer_url}
+
+            try:
+                response = requests.get(origin_url, stream=True, headers=headers)
+                response.raise_for_status()
+                image = cv2.imdecode(np.frombuffer(response.content, np.uint8), 1)
+                _, ext = os.path.splitext(img_path)
+
+                encode_param, img_type = self.get_encode_param_and_type(ext)
+
+                output = io.BytesIO()
+                result, data = cv2.imencode(ext, image, encode_param)
+                output.write(data)
+                output.seek(0)
+                self.send_response(200)
+                self.send_header('Content-type', img_type)
+                self.end_headers()
+                self.wfile.write(output.read())
+
+            except requests.exceptions.HTTPError as e:
+                self.send_error(400, message="Bad Request: {}".format(str(e)))
+
+            except Exception as e:
+                self.send_error(500, message="Internal Server Error: {}".format(str(e)))
 
     def get_encode_param_and_type(self, ext):
         if ext in [".jpg", ".jpeg"]:
