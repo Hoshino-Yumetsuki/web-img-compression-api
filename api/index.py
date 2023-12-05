0070a20ee98a5f7f86c52848a8604304418ca6b1
Anjiurine <q78kg.1ffrj6@outlook.com>
2023-12-05 20:38:41 +0800

    fix some bug

--- api/index.py
+++ api/index.py
@@ -4,7 +4,7 @@ import numpy as np
 import requests
 import io
 import os
-from urllib.parse import unquote, urlparse, parse_qs
+from urllib.parse import unquote
 import json
 
 class handler(BaseHTTPRequestHandler):
@@ -13,15 +13,7 @@ class handler(BaseHTTPRequestHandler):
         active_mode = os.environ.get("ACTIVE_MODE", "").lower() in ["true", "True"]
 
         if active_mode:
-            parsed_url = urlparse(self.path)
-            query_params = parse_qs(parsed_url.query)
-            img_url = query_params.get('img', [''])[0]
-
-            if not img_url:
-                self.send_error_response(400, "Bad Request", "Missing 'img' query parameter")
-                return
-
-            img_url = unquote(img_url)
+            img_url = unquote(self.path[2:])
             headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36 Edg/118.0.2088.76'}
 
             try:
@@ -49,7 +41,36 @@ class handler(BaseHTTPRequestHandler):
             except Exception as e:
                 self.send_error_response(500, "Internal Server Error", str(e))
         else:
-            self.send_error_response(400, "Bad Request", "Inactive mode is not supported")
+            img_path = unquote(self.path[1:])
+            env_origin_url = os.environ.get("ORIGIN_URL")
+            origin_url = env_origin_url + img_path
+            env_referer_url = os.environ.get("REFERER_URL")
+            headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36 Edg/118.0.2088.76', 'Referer': env_referer_url}
+
+            try:
+                response = requests.get(origin_url, stream=True, headers=headers)
+                response.raise_for_status()
+                image = cv2.imdecode(np.frombuffer(response.content, np.uint8), 1)
+                _, ext = os.path.splitext(img_path)
+
+                ext = ext.lower()
+
+                encode_param, img_type = self.get_encode_param_and_type(ext)
+
+                output = io.BytesIO()
+                result, data = cv2.imencode(ext, image, encode_param)
+                output.write(data)
+                output.seek(0)
+                self.send_response(200)
+                self.send_header('Content-type', img_type)
+                self.end_headers()
+                self.wfile.write(output.read())
+
+            except requests.exceptions.HTTPError as e:
+                self.send_error_response(400, "Bad Request", str(e))
+
+            except Exception as e:
+                self.send_error_response(500, "Internal Server Error", str(e))
 
     def send_error_response(self, status_code, error_type, message):
         self.send_response(status_code)
